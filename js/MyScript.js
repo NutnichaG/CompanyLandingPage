b8ae8c6486cd6f49f009cdad893b66c69066e613
Nutnicha <49384332+NutnichaG@users.noreply.github.com>
2022-06-07 01:19:34 +0100

    Add files via upload

---
+++ js/MyScript.js
@@ -0,0 +1,51 @@
+function shows1() {
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("Readmore1");
+
+  if (moreText1.style.display == 'none') {
+      moreText1.style.display = 'block';
+      btnText1.textContent = 'x';
+  } else {
+      moreText1.style.display = 'none';
+      btnText1.textContent = '+';
+  }
+}
+
+function shows2() {
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("Readmore2");
+
+  if (moreText2.style.display == 'none') {
+      moreText2.style.display = 'block';
+      btnText2.textContent = 'x';
+  } else {
+      moreText2.style.display = 'none';
+      btnText2.textContent = '+';
+  }
+}
+
+function shows3() {
+  var moreText3 = document.getElementById("more3");
+  var btnText3 = document.getElementById("Readmore3");
+
+  if (moreText3.style.display == 'none') {
+      moreText3.style.display = 'block';
+      btnText3.textContent = 'x';
+  } else {
+      moreText3.style.display = 'none';
+      btnText3.textContent = '+';
+  }
+}
+
+function shows4() {
+  var moreText4 = document.getElementById("more4");
+  var btnText4 = document.getElementById("Readmore4");
+
+  if (moreText4.style.display == 'none') {
+      moreText4.style.display = 'block';
+      btnText4.textContent = 'x';
+  } else {
+      moreText4.style.display = 'none';
+      btnText4.textContent = '+';
+  }
+}
